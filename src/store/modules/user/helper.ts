313dff6f52f280f3bcf85b874d2f7063dff84d2f
jason-xiaobaiai <147799771+jason-xiaobaiai@users.noreply.github.com>
2024-01-17 14:00:38 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://s2.loli.net/2024/01/02/pSmUPXzkRM4nIou.png',
+      name:  t('mjset.sysname'),//'ChatGPT Web',
+      description: 'Powered by <a href="https://nextai.date" class="text-blue-500" target="_blank" >NextAI</a>',
     },
   }
 }
