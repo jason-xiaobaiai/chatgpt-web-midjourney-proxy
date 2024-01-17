149560607a12b3d1e0ee0ffa3d2b9f6bc0bb723a
jason-xiaobaiai <147799771+jason-xiaobaiai@users.noreply.github.com>
2024-01-17 14:01:35 +0800

    Update drawList.vue

--- src/views/mj/drawList.vue
+++ src/views/mj/drawList.vue
@@ -61,7 +61,7 @@ async function onConversation() {
   if (loading.value)
     return
   if( !message.drawText && dataSources.value.length==0){
-      message.drawText=  t('mjset.sysname');//'AI绘图';
+      message.drawText=  t('mjset.sysname');//'ChatGPT Web';
   }
 //   if (!message || message.trim() === '')
 //     return
